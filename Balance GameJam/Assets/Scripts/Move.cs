c9e06927a501e00212562468885fae261e771148
MIIIIND <marcinkulesza.png@gmail.com>
2022-12-03 01:18:46 +0100

    init

--- Balance GameJam/Assets/Scripts/Move.cs
+++ Balance GameJam/Assets/Scripts/Move.cs
@@ -1,13 +1,23 @@
+using System.Collections;
 using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    public float moveSpeed;
+    public float moveSpeed; 
+    public float speedboost;
     public Rigidbody2D rb;
 
     private float keyMoveX = 0;
     private float keyMoveY = 0;
     private Vector3 velocity = Vector3.zero;
+
+    private bool canDash = true;
+    private bool isDashing;
+    public float dashingPower;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 1f;
+  //  [SerializeField] private TrailRenderer tr;
+
 
 
     private void Update()
@@ -16,16 +26,86 @@ public class Move : MonoBehaviour
         keyMoveY = 0;
         keyMoveX = Input.GetAxis("Horizontal");
         keyMoveY = Input.GetAxis("Vertical");
+        print(Input.GetAxis("Horizontal"));
+        print(Input.GetAxis("Vertical"));
+
+        if (Input.GetKeyDown(KeyCode.Space) && canDash) 
+        {
+            StartCoroutine(Dash());
+        }
+
+
     }
     
     private void FixedUpdate()
     {
-        MovePlayer(keyMoveX * moveSpeed * Time.deltaTime, keyMoveY * moveSpeed * Time.deltaTime);
+        
+        //if (Input.GetButtonDown("Jump"))
+        //{
+        // MovePlayer(keyMoveX * speedboost * Time.deltaTime, keyMoveY * speedboost * Time.deltaTime);
+        //    print("boos");
+        //}
+        //else
+        //{
+         MovePlayer(keyMoveX * moveSpeed * Time.deltaTime, keyMoveY * moveSpeed * Time.deltaTime);
+          
+        //}
+
+
     }
 
     private void MovePlayer(float Xmove, float Ymove)
     {
         Vector3 targetVelocity = new Vector2(Xmove,Ymove);
         rb.velocity = Vector3.SmoothDamp(rb.velocity,targetVelocity, ref velocity, .05f);
+    }
+
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+
+
+        if(keyMoveX > 0 && keyMoveY > 0) 
+        {
+            rb.velocity = new Vector2(transform.localScale.x * dashingPower, transform.localScale.y * dashingPower);
+        }
+        else if(keyMoveX > 0 && keyMoveY < 0)
+        {
+            rb.velocity = new Vector2(transform.localScale.x * dashingPower, transform.localScale.y * -dashingPower);
+        }
+        else if (keyMoveX < 0 && keyMoveY > 0)
+        {
+            rb.velocity = new Vector2(transform.localScale.x * -dashingPower, transform.localScale.y * dashingPower);
+        }
+        else if (keyMoveX < 0 && keyMoveY < 0)
+        {
+            rb.velocity = new Vector2(transform.localScale.x * -dashingPower, transform.localScale.y * -dashingPower);
+        }
+        else if (keyMoveX == 0 && keyMoveY < 0)
+        {
+            rb.velocity = new Vector2(transform.localScale.x * 0, transform.localScale.y * -dashingPower);
+        }
+        else if (keyMoveX == 0 && keyMoveY > 0)
+        {
+            rb.velocity = new Vector2(transform.localScale.x * 0, transform.localScale.y * dashingPower);
+        }
+        else if (keyMoveX < 0 && keyMoveY == 0)
+        {
+            rb.velocity = new Vector2(transform.localScale.x * -dashingPower, transform.localScale.y * 0);
+        }
+        else if (keyMoveX > 0 && keyMoveY == 0)
+        {
+            rb.velocity = new Vector2(transform.localScale.x * dashingPower, transform.localScale.y * 0);
+        }
+
+
+        //  tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+    //    tr.emitting = false;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 }
