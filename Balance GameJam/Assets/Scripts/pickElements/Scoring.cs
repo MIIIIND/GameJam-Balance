a6c3ff278c77be176f6c7aff9321369653f3de30
MIIIIND <marcinkulesza.png@gmail.com>
2022-12-05 13:32:20 +0100

    cringer

--- Balance GameJam/Assets/Scripts/pickElements/Scoring.cs
+++ Balance GameJam/Assets/Scripts/pickElements/Scoring.cs
@@ -13,20 +13,39 @@ public class Scoring : MonoBehaviour
     public event EventHandler OnHealthItem;
 
     public TilemapRenderer fireColorActivation;
+    public TilemapRenderer fireTreesActivaton;
+    public TilemapRenderer fireTreeTopsActivation;
+
     public TilemapRenderer waterColorActivation;
+    public TilemapRenderer waterTreesActivaton;
+    public TilemapRenderer waterTreeTopsActivation;
+
+
     public TilemapRenderer earthColorActivation;
+    public TilemapRenderer earthTreesActivation;
+    public TilemapRenderer earthTreeTopsActivation;
+
+
     public TilemapRenderer windColorActivation;
+    public TilemapRenderer windTreesActivation;
+    public TilemapRenderer windTreeTopsActivation;
+
+
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if(collision.CompareTag("Fire") && GameObject.Find("OffrandeFire") == null)
         {
             OnPickedFire?.Invoke(this,EventArgs.Empty);
             fireColorActivation.GetComponent<TilemapRenderer>().sortingOrder = -25;
+            fireTreesActivaton.GetComponent<TilemapRenderer>().sortingOrder = -24;
+            fireTreeTopsActivation.GetComponent<TilemapRenderer>().sortingOrder = 3;
         }
         else if(collision.CompareTag("Water") && GameObject.Find("OffrandeWater") == null)
         {
             OnPickedWater?.Invoke(this,EventArgs.Empty);
             waterColorActivation.GetComponent<TilemapRenderer>().sortingOrder = -25;
+            waterTreesActivaton.GetComponent<TilemapRenderer>().sortingOrder = -24;
+            waterTreeTopsActivation.GetComponent<TilemapRenderer>().sortingOrder = 3;
         }
         else if(collision.CompareTag("Emptiness") && GameObject.Find("OffrandeEmptiness") == null)
         {
@@ -36,11 +55,15 @@ public class Scoring : MonoBehaviour
         {
             OnPickedWind?.Invoke(this, EventArgs.Empty);
             windColorActivation.GetComponent<TilemapRenderer>().sortingOrder = -25;
+            windTreesActivation.GetComponent<TilemapRenderer>().sortingOrder = -24;
+            windTreeTopsActivation.GetComponent<TilemapRenderer>().sortingOrder = 3;
         }
         else if(collision.CompareTag("Earth") && GameObject.Find("OffrandeEarth") == null)
         {
             OnPickedEarth?.Invoke(this,EventArgs.Empty);
             earthColorActivation.GetComponent<TilemapRenderer>().sortingOrder = -25;
+            earthTreesActivation.GetComponent<TilemapRenderer>().sortingOrder = -24;
+            earthTreeTopsActivation.GetComponent<TilemapRenderer>().sortingOrder = 3;
         }
         else if (collision.CompareTag("Enemi"))
         {
